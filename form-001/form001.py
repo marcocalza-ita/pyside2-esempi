847b9be4c2a5c6784a8b08363b90e70fd4e61369
marco <smocu@tiscali.it>
2020-01-04 22:15:56 +0100

    aggiunto file form1

---
+++ form-001/form001.py
@@ -0,0 +1,22 @@
+from PySide2.QtWidgets import QApplication, QWidget
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Form1 di esempio")
+        self.setGeometry(300,300, 500,400)
+        self.setMinimumHeight(100)
+        self.setMinimumWidth(250)
+        self.setMaximumHeight(200)
+        self.setMaximumWidth(800)
+
+
+myApp = QApplication(sys.argv)
+window = Window()
+window.show()
+
+myApp.exec_()
+sys.exit(0)
